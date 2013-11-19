54baea5d94370475605b5d1705420fafa2649775
Yusuf Sutarko <yusufsutarko@gmail.com>
2013-11-19 16:53:25 +0700

    wqer

--- xpdc/JavaSource/com/sma/web/TransaksiSPController.java
+++ xpdc/JavaSource/com/sma/web/TransaksiSPController.java
@@ -38,7 +38,8 @@ import com.sma.validator.TransaksiSPValidator;
  * Controller untuk Transaksi SPP (Rincian Barang Terkirim) (Input, Print)
  * 
  * Test Comment untuk sync di GitHub
- * Test Comment 2
+ * 
+ * Test Comment untuk sync di GitHub
  * 
  * @author Yusuf
  * @since Jul 16, 2013 (10:55:26 AM)
